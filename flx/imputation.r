4cb4b31a083bf92fccd58dee4ae3d755c53c2b5f
Alexandru Meterez <alexandrumeterez@gmail.com>
2021-03-27 16:33:18 +0200

    [INFO] Imputed data using Amelia in R and updated gitignore

---
+++ flx/imputation.r
@@ -0,0 +1,11 @@
+library(Amelia)
+data_file <- "data/ddf_combined_mlflx_20210323.csv"
+df <- read.csv(data_file)
+keep_cols <- c("sitename", "date", "TA_F", "SW_IN_F", "LW_IN_F", "VPD_F", "PA_F",
+               "P_F", "WS_F", "USTAR", "CO2_F_MDS", "GPP_NT_VUT_REF")
+df <- df[keep_cols]
+
+idvars <- c("sitename", "date")
+ncpus = 8
+m = ncpus * 10      # 80 sets, see Bagging
+df.out = amelia(df, idvars = idvars, m = m, parallel = 'multicore', ncpus = ncpus)
