49c5a2da9e48001640525ab953471705e2903f2f
pdebartol <pdebartol@student.ethz.ch>
2021-04-27 10:40:43 +0200

    half/half sites

--- VAE/cv_half.py
+++ VAE/cv_half.py
@@ -23,7 +23,6 @@ parser.add_argument('-d', '--latent_dim', default=None, type=int,
 
 args = parser.parse_args()
 DEVICE = torch.device("cuda:" + args.gpu)
-DEVICE = "cpu"
 # Load Configs
 with open('config.json', 'r') as config_file:
     config_data = json.load(config_file)
@@ -50,17 +49,18 @@ INPUT_FEATURES = len(df_sensor[0].columns) + CONDITIONAL_FEATURES
 cv_r2 = []
 
 for s in tqdm(range(len(df_sensor))):
-  sites_to_train = list(range(len(df_sensor)))
-  sites_to_train.remove(s)
-  sites_to_test = [s]
-
-  x_train = [pd.concat([df_sensor[i],df_meta[i]],axis=1).values for i in sites_to_train]
-  conditional_train = [df_meta[i].values for i in sites_to_train]
-  y_train = [df_gpp[i].values.reshape(-1,1) for i in sites_to_train]
- 
-  x_test = [pd.concat([df_sensor[i],df_meta[i]],axis=1).values for i in sites_to_test]
-  conditional_test = [df_meta[i].values for i in sites_to_test]
-  y_test = [df_gpp[i].values.reshape(-1,1) for i in sites_to_test]
+  X = pd.concat([df_sensor[s],df_meta[s]],axis=1).values
+  Conditional = df_meta[s].values 
+  Y = df_gpp[s].values.reshape(-1,1)
+  
+  
+  x_train = X[:len(X)//2]
+  conditional_train =  Conditional[:len(Conditional)//2]
+  y_train =   Y[:len(Y)//2]
+  
+  x_test = X[len(X)//2:]
+  conditional_test = Conditional[len(Conditional)//2:]
+  y_test = Y[len(Y)//2:]
 
   encoder = EncoderWithTime(INPUT_FEATURES, ENCODER_OUTPUT_SIZE).to(DEVICE)
   reparam = Reparametrize(ENCODER_OUTPUT_SIZE, LATENT_SIZE).to(DEVICE)
@@ -73,33 +73,30 @@ for s in tqdm(range(len(df_sensor))):
   r2 = []
 
   for epoch in range(args.n_epochs):
-
       model.train()
-      for (x, y, conditional) in zip(x_train, y_train, conditional_train):
-        x = torch.FloatTensor(x).unsqueeze(1).to(DEVICE)
-        y = torch.FloatTensor(y).to(DEVICE)
-        conditional = torch.FloatTensor(conditional).to(DEVICE)
-        outputs, mean, logvar, y_pred = model(x, conditional)
-        x = x.squeeze(1)
+      x = torch.FloatTensor(x_train).unsqueeze(1).to(DEVICE)
+      y = torch.FloatTensor(y_train).to(DEVICE)
+      conditional = torch.FloatTensor(conditional_train).to(DEVICE)
+      outputs, mean, logvar, y_pred = model(x, conditional)
+      x = x.squeeze(1)
 
-        optimizer.zero_grad()
-        loss, recon_loss, kl_loss, reg_loss = loss_fn(outputs, x, y_pred, y, mean, logvar, 1)
-        loss.backward()
-        optimizer.step()
+      optimizer.zero_grad()
+      loss, recon_loss, kl_loss, reg_loss = loss_fn(outputs, x, y_pred, y, mean, logvar, 1)
+      loss.backward()
+      optimizer.step()
       
       model.eval()
       with torch.no_grad():
-          for (x, y, conditional) in zip(x_test, y_test, conditional_test):
-            x = torch.FloatTensor(x).unsqueeze(1).to(DEVICE)
-            y = torch.FloatTensor(y).to(DEVICE)
-            conditional = torch.FloatTensor(conditional).to(DEVICE)
+          x = torch.FloatTensor(x_test).unsqueeze(1).to(DEVICE)
+          y = torch.FloatTensor(y_test).to(DEVICE)
+          conditional = torch.FloatTensor(conditional_test).to(DEVICE)
 
-            outputs, mean, logvar, y_pred = model(x, conditional)
+          outputs, mean, logvar, y_pred = model(x, conditional)
 
-            x = x.squeeze(1)
+          x = x.squeeze(1)
             
-            loss, recon_loss, kl_loss, reg_loss = loss_fn(outputs, x, y_pred, y, mean, logvar, 1)
-            r2.append(r2_score(y_true=y.detach().cpu().numpy(), y_pred=y_pred.detach().cpu().numpy()))
+          loss, recon_loss, kl_loss, reg_loss = loss_fn(outputs, x, y_pred, y, mean, logvar, 1)
+          r2.append(r2_score(y_true=y.detach().cpu().numpy(), y_pred=y_pred.detach().cpu().numpy()))
   cv_r2.append(max(r2))
   print(f"Test Site: {data.index.unique()[s]} R2: {cv_r2[s]}")
   print("CV R2 cumulative mean: ", np.mean(cv_r2), " +- ", np.std(cv_r2))
