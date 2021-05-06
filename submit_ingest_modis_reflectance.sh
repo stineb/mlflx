d249a5fca24dc6e51cfb773fd533c2e2020bedd6
stineb <benjamin.stocker@gmail.comm>
2021-05-06 08:22:42 +0200

    asdf

--- submit_ingest_modis_reflectance.sh
+++ submit_ingest_modis_reflectance.sh
@@ -2,10 +2,10 @@
 
 module load eth_proxy
 
-# njobs=69
-# for ((n=1;n<=${njobs};n++)); do
-#     echo "Submitting chunk number $n ..."
-#     bsub -W 72:00 -u bestocke -J "ingest_modis_reflectance $n" -R "rusage[mem=5000]" "Rscript --vanilla ingest_modis_reflectance.R $n"
-# done
+njobs=1  #69
+for ((n=1;n<=${njobs};n++)); do
+    echo "Submitting chunk number $n ..."
+    bsub -W 72:00 -u bestocke -J "ingest_modis_reflectance $n" -R "rusage[mem=5000]" "Rscript --vanilla ingest_modis_reflectance.R $n"
+done
 
-bsub -W 72:00 -u bestocke -J "ingest_modis_reflectance $n" -R "rusage[mem=5000]" "Rscript --vanilla ingest_modis_reflectance.R 5"
+# bsub -W 72:00 -u bestocke -J "ingest_modis_reflectance $n" -R "rusage[mem=5000]" "Rscript --vanilla ingest_modis_reflectance.R 5"
